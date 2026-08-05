53cfb1aa6edcbb545ef396ce0742412a7579b229
Josiel Linhar <iamlinhar@gmail.com>
2026-08-04 23:07:41 -0300

    adicionado componente card no main

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,9 +1,12 @@
 import { Card } from "../components/card";
+import { CardContentLeft } from "../components/cardContent/CardContentLeft";
+import { CardContentRight } from "../components/cardContent/CardContentRight";
 
 export function Main() {
   return (
-    <div>
-      <Card />
+    <div className="px-5 grid grid-cols-2 gap-5">
+      <Card title="SQL Builder" cardContent={CardContentLeft()} />
+      <Card title="Generated SQL" cardContent={CardContentRight()} />
     </div>
-  )
+  );
 }
