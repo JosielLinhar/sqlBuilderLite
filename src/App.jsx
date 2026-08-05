95770adf5fa23322cd89c2593a5413e2e3831e9b
Josiel Linhar <iamlinhar@gmail.com>
2026-08-04 23:02:39 -0300

    ajustes da pagina principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from "react"
-import { Button } from "../src/components/ui/button"
 import { Header } from "./layout/Header";
 import { Main } from "./layout/Main";
 
 function App() {
-  const [num, setNum] = useState(0);
-
   return (
     <div className="h-svh w-full bg-[#09090B] text-white">
       <Header />
